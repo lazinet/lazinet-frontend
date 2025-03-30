9e503961e0aed6b0fa0cc4274c0d219b0153b8ed
lazinet <lazinet.com@gmail.com>
2025-03-30 20:05:00 +0700

    Blog update

--- public/assets/js/lazinet.js
+++ public/assets/js/lazinet.js
@@ -1,3 +1,16 @@
-function toggleFlip(card) {
-    card.classList.toggle('flipped');
-  }  
+// function toggleFlip(card) {
+//     card.classList.toggle('flipped');
+//   }  
+
+// function googleTranslateElementInit() {
+      //   new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
+      // }
+    
+      function googleTranslateElementInit() {
+        new google.translate.TranslateElement({
+          pageLanguage: 'en',
+          layout: google.translate.TranslateElement.InlineLayout.SIMPLE,
+          "includedLanguages": "vi,zh-CN,hi,es,fr,ar,ru,pt,ja,ko,id,ms,th,my,km,lo,tl,de,it,fa,ur,pl,tr,uk,ro,nl,sv,cs,he,el,hu,da,fi",
+          autoDisplay: false
+        }, 'google_translate_element');
+      }
